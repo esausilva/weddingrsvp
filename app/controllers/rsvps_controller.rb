9a0eda3124bff3a2520be373d3c9e6ae919110c5
Esau Silva <esausilva85@gmail.com>
2016-01-09 18:47:04 -0600

    added rsvp model, validations, handling rsvp submission and some styling

--- app/controllers/rsvps_controller.rb
+++ app/controllers/rsvps_controller.rb
@@ -1,9 +1,28 @@
 class RsvpsController < ApplicationController
+  before_action :set_rsvp, only: [:show, :destroy]
+
   def new
     @rsvp = Rsvp.new
   end
 
   def create
+    @rsvp = Rsvp.new(rsvp_params)
 
+    if @rsvp.save
+      flash[:success] = 'Thank you'
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def rsvp_params
+    params.require(:rsvp).permit(:party, :attendees, :comment, :accept)
+  end
+
+  def set_rsvp
+    @rsvp = Rsvp.find(params[:id])
   end
 end
--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -1,3 +1,6 @@
 class Rsvp < ActiveRecord::Base
-  
+  validates :party, presence: true, length: { minimum: 5 }
+  validates :attendees, presence: true
+  #validates :accept, presence: true
+  validates :comment, length: { minimum: 15 }
 end
